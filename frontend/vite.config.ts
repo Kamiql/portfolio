3a24ec19df7602039cf632839736186ba0ea1754
kamiql <kilianaqua08@gmail.com>
2025-07-10 18:59:23 +0200

    Backendddd... pllsssss

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,15 @@
+// frontend/vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig(({}) => ({
   base: '/',
-  plugins: [react(), tailwindcss()],
+  plugins: [react(),tailwindcss(),],
   server: {
     port: 5173,
     proxy: {
-      '^/api(/.*)?$': {
+      '^/api/.*': {
         target: 'http://localhost:8000',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
