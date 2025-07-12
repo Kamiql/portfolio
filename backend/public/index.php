9afb806c93caffc8b9574479b948050de108c66c
kamiql <kilianaqua08@gmail.com>
2025-07-12 17:11:31 +0200

    Changed php stuff

--- backend/public/index.php
+++ backend/public/index.php
@@ -1,28 +1,21 @@
 <?php
 
-declare(strict_types=1);
-
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
-use Slim\Factory\AppFactory;
-
 require __DIR__ . '/../vendor/autoload.php';
 
-$app = AppFactory::create();
-$app->addRoutingMiddleware();
+use Leaf\Router;
 
-$app->get('/assets/{file:.+}', function (Request $req, Response $res, array $args) {
-    $filePath = __DIR__ . '/assets/' . $args['file'];
+Router::get('/test', fn () => print('Hello from Leaf!'));
 
-    if (!file_exists($filePath) || !is_file($filePath)) {
-        return $res->withStatus(404)->write('File not found');
+Router::get('/test2', function () {
+    $image = __DIR__ . '/assets/kamiql.png';
+    if (!file_exists($image)) {
+        http_response_code(404);
+        exit('Image not found');
     }
-
-    $mimeType = mime_content_type($filePath);
-    $res = $res->withHeader('Content-Type', $mimeType);
-
-    $res->getBody()->write(file_get_contents($filePath));
-    return $res;
+    header('Content-Type: image/png');
+    readfile($image);
 });
 
-$app->run();
+Router::set404(fn () => exit('Route not found'));
+
+Router::run();
